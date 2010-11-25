b5f20a68dd2f02b763db626ae1bfa07a5932e87f
Sammy Hubner <ceo@sammy.nu>
2010-11-25 20:51:42 +0100

    added google analytics code

--- front-page.php
+++ front-page.php
@@ -161,6 +161,16 @@
 	<script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 	<!-- end concatenated and minified scripts-->
 
+	<script>
+	   var _gaq = [['_setAccount', 'UA-19803958-1'], ['_trackPageview']];
+	   (function(d, t) {
+	    var g = d.createElement(t),
+	        s = d.getElementsByTagName(t)[0];
+	    g.async = true;
+	    g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    s.parentNode.insertBefore(g, s);
+	   })(document, 'script');
+	  </script>
 	<?php
 		/* Always have wp_footer() just before the closing </body>
 		 * tag of your theme, or you will break many plugins, which
