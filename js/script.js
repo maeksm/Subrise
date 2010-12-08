e0148e9fe59b1d76e17f26396a0009c0c16593ae
Sammy Hubner <ceo@sammy.nu>
2010-12-08 16:41:15 +0100

    Added temporary konami code

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@
 
 */
 
+// KONAMI CODE WOOOOOTTTT
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function(e) {
+	kkeys.push( e.keyCode );
+	if ( kkeys.toString().indexOf( konami ) >= 0 ){
+		$(document).unbind('keydown',arguments.callee);
+		$.getScript('http://www.somethinghitme.com/wp-content/themes/somethinghitme/js/snowfall.min.jquery.js',function(){
+			alert("Merry Christmas");
+			$(document).snowfall({flakeCount: 100, maxSpeed:10});
+		});          
+  }
+});
+
+
 $(document).ready(function() {
 	$('#contentCarousel').roundabout({
 		reflect:true,
